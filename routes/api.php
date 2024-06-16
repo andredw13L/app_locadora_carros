8a60358f8efdd55c3036e8c23fdf8cbc16115dcb
andredw13L <andreribeiro13l@gmail.com>
2024-06-16 18:50:37 -0300

    Entendendo o grupo de rotas Web e API e a importância do Content-Type

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,17 @@
-/* php artisan install:api
-   api não é mais padrão a partir do Laravel 11
-*/
-
-
 <?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/* php artisan install:api
+   api não é mais padrão a partir do Laravel 11
+*/
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/', function () {
+    return ['Chegamos até aqui' => 'SIM'];
+});
