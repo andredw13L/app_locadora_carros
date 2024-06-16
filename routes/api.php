548152b84b545bd7836dba28f163d7314c1624ef
andredw13L <andreribeiro13l@gmail.com>
2024-06-16 19:08:10 -0300

    Rotas apiResource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\LocacaoController;
+use App\Http\Controllers\MarcaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +15,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::get('/', function () {
-    return ['Chegamos até aqui' => 'SIM'];
-});
+Route::apiResource('cliente', ClienteController::class);
+Route::apiResource('carro', CarroController::class);
+Route::apiResource('locacao', LocacaoController::class);
+Route::apiResource('marca', MarcaController::class);
+Route::apiResource('modelo', ClienteController::class);
