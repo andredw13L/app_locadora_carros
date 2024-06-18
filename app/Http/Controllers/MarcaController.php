94d4f1b0c154205997ba8ad8b44b8403f08ca33b
andredw13L <andreribeiro13l@gmail.com>
2024-06-18 01:55:45 -0300

    Removendo registros via DELETE

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -47,6 +47,7 @@ class MarcaController extends Controller
      */
     public function destroy(Marca $marca)
     {
-        //
+        $marca->delete();
+        return ['msg' => 'A marca foi removida com sucesso'];
     }
 }
