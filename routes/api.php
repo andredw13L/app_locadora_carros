baf607b13aa654aa30e3151f2a497a58b15eeb04
andredw13L <andreribeiro13l@gmail.com>
2024-07-06 00:46:43 -0300

    Rota referenciava o controller errado

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\CarroController;
-use App\Http\Controllers\ClienteController;
-use App\Http\Controllers\LocacaoController;
-use App\Http\Controllers\MarcaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ModeloController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\LocacaoController;
 
 /* php artisan install:api
    api não é mais padrão a partir do Laravel 11
@@ -19,4 +20,4 @@ Route::apiResource('cliente', ClienteController::class);
 Route::apiResource('carro', CarroController::class);
 Route::apiResource('locacao', LocacaoController::class);
 Route::apiResource('marca', MarcaController::class);
-Route::apiResource('modelo', ClienteController::class);
+Route::apiResource('modelo', ModeloController::class);
