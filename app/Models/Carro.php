37b70a67830e484ab40a48afb8f76f699f83bfb3
andredw13L <andreribeiro13l@gmail.com>
2024-07-07 20:25:14 -0300

    API WebService Rest para o Resource Carro

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Modelo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Carro extends Model
 {
     use HasFactory;
+    protected $fillable = ['modelo_id', 'placa', 'disponivel', 'km'];
+
+    public function rules() {
+        return [
+            'modelo_id' => 'exists:modelos,id',
+            'placa' => 'required',
+            'disponivel' => 'required',
+            'km' => 'required'
+        ];
+    }
+
+    public function modelo(): BelongsTo {
+        return $this->belongsTo(Modelo::class);
+    }
 }
