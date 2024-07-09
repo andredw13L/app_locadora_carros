18d11eda5c28793ae354b98884f676bc953b5f5f
andredw13L <andreribeiro13l@gmail.com>
2024-07-09 01:05:32 -0300

    Implementando o método de login (autenticação) e o JWT (autorização)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,25 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-    public function login() {
+    public function login(Request $request) {
+
+        $credenciais = $request->all(['email', 'password']);
+
+        // Autenticação (email e senha)
+        $token = auth('api')->attempt($credenciais);
+
+        if($token) {
+            return response()->json(['token' => $token]);
+            
+        } else {
+            return response()->json(
+                ['erro' => 'Usuário ou senha inválido!']
+                ,401
+            );
+
+            // 401 = Unauthorized
+        }
+
         return 'login';
     }
 
