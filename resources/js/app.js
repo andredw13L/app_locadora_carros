67cf4aa2130cf239706fb3e3a490b0fe42a5f768
andredw13L <andreribeiro13l@gmail.com>
2024-07-09 04:50:49 -0300

    Convertendo a view login em um componente Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,6 +18,9 @@ const app = createApp({});
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
 
+import LoginVue from './components/Login.vue';
+app.component('login-component', LoginVue);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
