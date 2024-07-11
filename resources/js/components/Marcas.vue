74084002dedf35679e0aefd79a51834158e147f6
andredw13L <andreribeiro13l@gmail.com>
2024-07-11 02:13:51 -0300

    Criando um componente Vue para cards

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -3,11 +3,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
 
-
                 <!-- Início do card de busca -->
-                <div class="card mb-3">
-                    <div class="card-header">Busca de marcas</div>
-                    <div class="card-body">
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
                         <div class="row g-3">
                             <div class="col mb-3">
                                 <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca">
@@ -20,25 +18,24 @@
                                 </input-container-component>     
                             </div>
                         </div>
-                    </div>
-                    <div class="card-footer d-flex">
+                    </template>
+
+                    <template v-slot:rodape>
                         <button type="submit" class="btn btn-primary btn-sm ms-auto float-right">Pesquisar</button>
-                    </div>
-                </div>
+                    </template>
+                </card-component>
                 <!-- Fim do card de busca -->
 
                 <!-- Início do card de listagem de marcas -->
-
-                <div class="card">
-                    <div class="card-header">Relação de marcas</div>
-                    <div class="card-body">
+                 <card-component titulo="Relação de marcas">
+                    <template v-slot:conteudo>
                         <table-component></table-component>
-                    </div>
-                    <div class="card-footer d-flex">
-                        <button type="button" class="btn btn-primary btn-sm ms-auto float-right">Adicionar</button>
-                    </div>
-                </div>
+                    </template>
 
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm ms-auto float-right">Adicionar</button>
+                    </template>
+                 </card-component>
                 <!-- Fim do card de listagem de marcas -->
             </div>
         </div>
