e104c2d4f78f4eb4ef98cb12a058207a84e36ca4
andredw13L <andreribeiro13l@gmail.com>
2024-07-12 01:30:07 -0300

    Cadastrando marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -47,23 +47,59 @@
             <template v-slot:conteudo>
                 <div class="form-group">
                     <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Informe o nome da marca">
-                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca">
-                    </input-container-component>     
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca" v-model="nomeMarca">
+                    </input-container-component>  
+                    {{ nomeMarca }}   
                 </div>
                 <div class="form-group">
                     <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp" texto-ajuda="Selecione uma imagem no formato PNG">
-                        <input type="file" class="form-control" id="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem">
-                    </input-container-component>     
+                        <input type="file" class="form-control" id="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem" @change="carregarImagem($event)">
+                    </input-container-component>
+                    {{ arquivoImagem }}     
                 </div>
             </template>
             <template v-slot:rodape>
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary">Salvar</button>
+                <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
             </template>
         </modal-component>
     </div>
 </template>
 
 <script>
+    export default {
+        data() {
+            return {
+                nomeMarca: '',
+                arquivoImagem: []
+            }
+        },
 
+        methods: {
+            carregarImagem(e) {
+                this.arquivoImagem = e.target.files
+            },
+
+            salvar() {
+
+                const formData = new FormData()
+                formData.append('nome', this.nomeMarca)
+                formData.append('imagem', this.arquivoImagem[0])
+
+                const config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Accept': 'application/json'
+                    }
+                }
+
+                axios.post('http://localhost:8000/api/v1/marca', formData, config)
+                    .then(response => {
+                        console.log(response);
+                    })   
+                    .catch((errors) => console.log(errors));              
+            }
+            
+        }
+    }
 </script>
