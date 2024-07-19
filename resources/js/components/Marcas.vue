2dd550bb0818c0b25c12bed0aa9c061bb7e041cd
andredw13L <andreribeiro13l@gmail.com>
2024-07-19 02:12:02 -0300

    Adicionado o caractere % para as pesquisas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -145,7 +145,7 @@
                             filtro += ";"
                         }
 
-                        filtro += chave + ':like:' + this.busca[chave]
+                        filtro += chave + ':like:' + `%${this.busca[chave]}%`
                     }   
                     
                 }
